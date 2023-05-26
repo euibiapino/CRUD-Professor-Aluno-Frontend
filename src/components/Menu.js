5073756d176e8a5068a36f0b265556053e04bae7
eubiapino <ibiapinojoaovitor@gmail.com>
2023-05-26 10:44:32 -0300

    Frontend do projeto

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Menu = () => {
+  return (
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: "100vh" }}
+    >
+    </div>
+  );
+};
